96ab23425e7852f3b37429fa5e53947dcbd62a89
Woopi <109776716+Samueljkauff@users.noreply.github.com>
2025-02-05 19:17:44 -0500

    initial commit. floating gui added

---
+++ imports/ui/App.vue
@@ -0,0 +1,22 @@
+
+<template>
+  <div class="m-8">
+    <AppMenu />
+    <GuiBox>
+      
+    </GuiBox>
+
+  </div>
+</template>
+
+
+
+<script setup>
+import GuiBox from './GuiBox.vue';
+
+
+</script>
+
+<style>
+
+</style>
---
+++ imports/ui/GuiBox.vue
@@ -0,0 +1,12 @@
+<template class="overscroll-none">
+    <div class="flex flex-row-reverse h-screen">
+        <div class="w-2/5 h-[93.5%] rounded-xl bg-gray-900">
+            
+        </div>
+        
+    </div>
+</template>
+
+<script setup>
+    
+</script>
